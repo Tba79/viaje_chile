810164724ca9d88be1b4a90ae16a10d368ba5c74
Esteban Vergara <estvergara@gmail.com>
2024-03-09 21:58:44 -0300

    se agrego FOOTER y Script para navegacion scroll slow

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,24 @@
-const navEl =document.querySelector('.navbar');
+const navEl = document.querySelector('.navbar');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY >= 60) {
+    if (window.scrollY >= 250) {
         navEl.classList.add('navbar-scroll');
-    } else if (window.scrollY < 56) {
+    } else if (window.scrollY < 250) {
         navEl.classList.remove('navbar-scroll')
     }
+});
+
+
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
 });
